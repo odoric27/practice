b2572492de445c0bc56e22d753768ab7fe05aa3a
odoric27 <dmo294@nyu.edu>
2017-02-07 18:14:41 -0500

    resolves #3 w/ recursive solution

--- Test.java
+++ Test.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
 public class Test {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-    //exception handling to come later
-    for(int i = 0; i < n; i++)
-      System.out.println("Hello");
-  }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		String s = "Hello";
+		//exception handling to come later
+		multiPrint(n, s);
+	}
+	public static void multiPrint(int n, String s) {
+  		if(n == 0)
+			return;
+		else {
+			System.out.println(s);
+			multiPrint(n-1, s);
+		}
+			
+	}
 }
