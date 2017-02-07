bcd44f0af121bcc4b1ff8630b3a508446fe3fa68
odoric27 <dmo294@nyu.edu>
2017-02-07 18:02:45 -0500

    resolves #3 w/ iterative solution

--- Test.java
+++ Test.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Test {
   public static void main(String[] args) {
-    System.out.println("Hello");
-    System.out.println("Hello");
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    //exception handling to come later
+    for(int i = 0; i < n; i++)
+      System.out.println("Hello");
   }
 }
